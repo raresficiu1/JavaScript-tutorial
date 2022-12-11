6d70a05c19165e18737ca0764e5b93aeb0a61730
Rares <raresficiu1@gmail.com>
2022-12-11 18:22:15 +0000

    no comment

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,42 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = '23';
+
+if (x == 23) console.log(23);
+
+var y = "'32'";
+
+console.log(y);
+console.log(y);
+console.log(y);
+
+console.log(y);
+console.log(y);
+console.log(y);
+
+console.log(y);
+console.log(y);
+console.log(y);
+
+console.log(y);
+console.log(y);
+console.log(y);
+
+y = "'33'";
+
+console.log(y);
+console.log(y);
+console.log(y);
+
+console.log(y);
+console.log(y);
+console.log(y);
+
+console.log(y);
+console.log(y);
+console.log(y);
+
+console.log(y);
+console.log(y);
+console.log(y);
